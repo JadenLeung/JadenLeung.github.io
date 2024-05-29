d964f97c0a186e48a1ef56b69309c07d119a24c3
JadenLeung <jaden.leung@gmail.com>
2024-05-29 13:56:23 -0400

    pls work

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/Jaden-portfolio/",
+  base: "/Jaden-portfolio/",
   plugins: [react()],
   css: {
     modules: {
