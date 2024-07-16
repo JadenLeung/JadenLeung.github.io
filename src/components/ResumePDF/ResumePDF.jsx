6230641c3c3dbe7473c8bdd01831956c74757855
JadenLeung <jaden.leung@gmail.com>
2024-07-15 23:59:17 -0400

    fixed resume

--- src/components/ResumePDF/ResumePDF.jsx
+++ src/components/ResumePDF/ResumePDF.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { getImageUrl } from '../../utils';
 import styles from './ResumePDF.module.css';
 
 export const ResumePDF = () => {
   return (
-    <iframe frameBorder="0" width="100%" height="1200px" src="/Resume7-16.pdf" className={styles.iframe}></iframe>
+    <iframe frameBorder="0" width="100%" height="1200px" src={getImageUrl("resume/Resume7-16.pdf")} className={styles.iframe}></iframe>
   )
 }
 
