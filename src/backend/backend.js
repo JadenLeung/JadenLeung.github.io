a9ea6ad54b985b1a9f2c4d0e000287ff8c527575
JadenLeung <jaden.leung@gmail.com>
2024-10-17 20:30:17 -0400

    Better tracking

---
+++ src/backend/backend.js
@@ -0,0 +1,43 @@
+
+export async function getIP() {
+	try {
+		const response = await fetch('https://api.ipify.org?format=json');
+		const data = await response.json();
+		return data.ip;
+	} catch (error) {
+		console.error("Error fetching IP:", error);
+		return "unknown";
+	}
+}
+
+function getDate () {
+  // Get the current date and time
+  const now = new Date();
+
+  // Format the date and time for the Eastern Time Zone
+  const options = { timeZone: 'America/New_York', hour12: false, year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  const formatter = new Intl.DateTimeFormat('en-US', options);
+  const formattedDate = formatter.format(now);
+
+  console.log(formattedDate);  // Output: MM/DD/YYYY, HH:MM:SS
+
+return formattedDate;
+}
+
+export async function modeData(mode) {
+  const IP = await getIP();
+  const TIME = getDate();
+  // console.log("IP is", IP, "TIME is", TIME);
+  fetch("https://elephant4.azurewebsites.net/api/portfolio", {
+    method: "POST",
+    body: JSON.stringify([{
+      ipaddr: IP,
+      mode: mode,
+      time: TIME
+    }]),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+    .then((response) => console.log(response));
+}
