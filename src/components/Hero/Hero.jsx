18059b7a0f713da943c5321d2d8706c212b964f4
JadenLeung <jaden.leung@gmail.com>
2025-04-16 14:57:20 -0400

    more changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export const Hero = () => {
     return (<section className = {styles.container}>
         <div className = {styles.content}>
             <h1 className = {styles.title}>Hi, I'm Jaden</h1>
-            <p className={styles.description}>I am a sophomore currently attending the University of Waterloo majoring in CS.</p>
+            <p className={styles.description}>I am a sophomore CS Student at the University of Waterloo.</p>
             <a className={styles.contactBtn} href="mailto:j28leung@uwaterloo.ca">Contact me</a>
         </div>
         <img className = {styles.heroImg} src={getImageUrl('hero/jaden.jpeg')} alt="Hero image of me"/>
